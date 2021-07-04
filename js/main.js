5e33caa8195c0296c4e4f5d933ec2a95567e0a54
Akanksha <akanksha.mohanty@saint-gobain.com>
2021-07-04 14:46:08 +0530

    additionalfunctionality

--- js/main.js
+++ js/main.js
@@ -43,6 +43,7 @@ function clearData(){
     colorLable.value = '';
     btnColor.style.backgroundColor = '';
     labelPreview.style.backgroundColor = '';
+    labelPreview.innerHTML = 'Label Preview 1';
     changeState();
 }
 function openDialog(){
@@ -52,6 +53,14 @@ function openDialog(){
         createLabelSection.style.display = "flex";
     }else{
         createLabelSection.style.display = "none";
+    }
+}
+function counterNumber(){
+    let count = table.tBodies[0].rows.length;   
+    rowCount.innerHTML = count;
+    console.log('count', count);
+    if(count >= 1){
+        clearData();
     }
 }
 function saveData(){
@@ -69,33 +78,11 @@ function saveData(){
     td4.innerHTML = '<button class="update-btn-group__link" type="button" onclick="deleteRow(this)">Delete</button>';
     tbody.appendChild(tr);
     console.log('tbody tr', tbody.appendChild(tr));
-    let count = table.tBodies[0].rows.length;   
-    rowCount.innerHTML = count;
-    console.log('count', count);
-    if(count >= 1){
-        clearData();
-    }
-}
-function search(){
-    console.log('count',table.tBodies[0].rows[0].cells[0].innerHTML);
-    let keyword = '';
-    let count = table.tBodies[0].rows.length;  
-    for(let i=0; i< count; i++){
-        keyword = table.tBodies[0].rows[i].cells[0].innerHTML;      
-    }
-    let data = table.tBodies[0].rows;
-    console.log('keyword', keyword);
-    keyword = keyword.toLowerCase();
-    let searchKey = searchItem.value;
-    console.log('data',data[0]);
-    let filtered_array = data.filter((row, index) => {
-        return (row.cells[0].toLowerCase(searchKey) != -1) || (item.cells[1].toLowerCase(searchKey) != -1);
-    });
-    console.log('filtered Array', filtered_array);
-
+    counterNumber();
 }
 function updateRow(e){
     let rowId = e.parentNode.parentNode;
+    console.log("rowId", rowId);
     let tr = document.querySelector('tr');
     let data = rowId.querySelectorAll('td');
     var name = data[0].innerHTML;
@@ -103,62 +90,55 @@ function updateRow(e){
     alert("Name:"+ name + "color: "+ color);
     inputValue.value = name;
     colorLable.value = color;
-    changeState();
     saveBtn.innerHTML = "Update Label";
     createLabelSection.style.display = "flex";
-    
-    tbody.replaceChild(rowId, inputValue.value);
-    // let tr =  document.createElement('tr');
-    // let td1 = tr.appendChild(document.createElement('td')); 
-    // let td2 = tr.appendChild(document.createElement('td')); 
-    // let td3 = tr.appendChild(document.createElement('td')); 
-    // let td4 = tr.appendChild(document.createElement('td')); 
-    // td1.innerHTML= '<input type="text" name="input3" id="input1" class="form-control form-control_size">';
-    // td2.innerHTML = '<input type="text" name="input4" id="input2" class="form-control form-control_size">';
-    // td3.innerHTML = '<button class="update-btn-group__link" type="button" onclick="addRow(this)">Update</buton>';
-    // td4.innerHTML = '<button class="update-btn-group__link" type="button" onclick="deleteRow(this)">Delete</button>';
-    // tbody.replaceChild(tr, pNode);
-}
-function addRow(e){
-    let pNode = e.parentNode.parentNode;
-    let tr =  document.createElement('tr');
-    let td1 = tr.appendChild(document.createElement('td')); 
-    let td2 = tr.appendChild(document.createElement('td')); 
-    let td3 = tr.appendChild(document.createElement('td')); 
-    let td4 = tr.appendChild(document.createElement('td')); 
-    td1.innerHTML= input1.value;
-    td2.innerHTML = input2.value;
-    td3.innerHTML = '<button class="update-btn-group__link" type="button" onclick="updateRow(this)">Edit</buton>';
-    td4.innerHTML = '<button class="update-btn-group__link" type="button" onclick="deleteRow(this)">Delete</button>';
-    document.querySelector('#tbody').replaceChild(tr, pNode);
+    console.log(rowId);
+    tbody.replaceChild(tr , rowId);
+    changeState();
 }
 function deleteRow(e){
     let confirmMessage = confirm('Are you sure you want to delete?');
     let del = e.parentNode.parentNode;
+    let count = table.tBodies[0].rows.length;  
     if(confirmMessage){
         console.log('parent node', e.parentNode.parentNode);
         console.log('delete parentNode', del.parentNode);
-        del.parentNode.removeChild(del);
+        del.parentNode.removeChild(del); 
+        rowCount.innerHTML = count - 1;
     }else{
         return false;
     }
 }
-function searLabel(){
+function search(){
+    let rowCell, textValue;
     let keyword = document.querySelector('#search').value;
     keyword = keyword.toLowerCase();
+    for(let i = 0; i <= table.tBodies[0].rows.length; i++){
+        rowCell = table.tBodies[0].rows[i].cells[0];
+        if(rowCell){
+            textValue = rowCell.textContent || rowCell.innerText;
+            if(textValue.toLowerCase().indexOf(keyword) > -1){
+                table.tBodies[0].rows[i].style.display = '';
+            }else{
+                table.tBodies[0].rows[i].style.display = 'none'; 
+            }
+        }
+
+        console.log(rowCell);
+    }
+    //rowCell = table.tBodies[0].rows[i].cells[0];
+    // let rows = table.tBodies[0].rows;
+    // let filteredRow = rows.filter((item) => {
+    //     if(item.cells[0]){
+    //         textValue = item.cells[0].innerText || item.cell[0].textContent;
+    //         if(textValue.toLowerCase().indexOf(keyword) > -1){
+    //            return item.style.display = '';
+    //         }else{
+    //           return  item.style.display = 'none';
+    //         }
+    //     }
+    //     console.log(item);
+    // });
+    // return filteredRow;
 }
-
-
-// let row = 1;
-// function saveData(){
-//     let newRow = table.insertRow(row);
-//     let cell1 = newRow.insertCell(0);
-//     let cell2 = newRow.insertCell(1);
-//     cell1.innerHTML = inputValue.value;
-//     if(inputValue.value == style.backgroundColor){
-
-//     }
-//     cell2.innerHTML = colorLable.value;
-//     console.log('button is clicked');
-//     row++;
-// }
+search();
